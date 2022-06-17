93ec2bc22f30e460725be293352b4edb61715ad0
MarcinkEve <marcinke@gmail.com>
2022-06-18 01:13:49 +0300

    'App.jsx' component Route paths and elements setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Layout from "./components/Layout/Layout";
+import Welcome from "./components/Welcome/Welcome";
+import NewUser from "./components/NewUser/NewUser";
+import NewCategory from "./components/NewCategory/NewCategory";
+// import UserCategories from "./components/UserCategories/UserCategories";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route>
-          <Route />
-          <Route />
-          <Route />
+        <Route path="/" element={<Layout />}>
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/newUser" element={<NewUser />} />
+          <Route path="/newCategory" element={<NewCategory />} />
+          {/* <Route path="/userCategories" element={<UserCategories />} /> */}
         </Route>
       </Routes>
     </>
