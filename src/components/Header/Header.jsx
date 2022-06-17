eb478b1c6b37f61c41249cdba0f9ac1522889f7d
MarcinkEve <marcinke@gmail.com>
2022-06-18 01:06:11 +0300

    'Header' component edited, still empty

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import "./header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+    Header
+    </>
+  );
+};
 
-export default Header
+export default Header;
