f9b89e45351749fca397654291236316f2d414c5
MarcinkEve <marcinke@gmail.com>
2022-06-18 01:04:57 +0300

    'Nav' component added to 'Layout' component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
-import Header from "../Header";
-import "./Layout.css";
+import Header from "../Header/Header";
+import Nav from "../Nav/Nav";
+import "./layout.css";
 
 const Layout = () => (
   <div>
     <Header />
     <div>
-      <Outlet />
+      <Nav />
+      <Outlet/>
     </div>
     <Footer />
   </div>
