b7c1d4e4ec5f4b901952de6e91d002c9d10fe32e
MarcinkEve <marcinke@gmail.com>
2022-06-18 00:56:57 +0300

    Component 'Nav' created

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./nav.css";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">
+            <p>Home</p>
+          </Link>
+        </li>
+        <li>
+          <Link to="/newUser">
+            <p>New User</p>
+          </Link>
+        </li>
+        <li>
+          <Link to="/newCategory">
+            <p>New Category</p>
+          </Link>
+        </li>
+        <li>
+          <Link to="/userCategories">
+            <p>User Categories</p>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
