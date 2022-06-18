97ad3a0f7ada6884eb355a7efb41dc2ce43d7351
MarcinkEve <marcinke@gmail.com>
2022-06-19 00:33:32 +0300

    Very untidy "newUser" code- to be continued...

--- src/components/NewUser/NewUser.jsx
+++ src/components/NewUser/NewUser.jsx
@@ -1,9 +1,331 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import "./newUser.css";
+import { Link } from "react-router-dom";
+import Data from "../Data";
+import Modal from "../Modal";
+
+const GENDER = ["Female", "Male", "Other"];
+const CATEGORIES = ["By plain", "By bus", "By ship"];
+const SUBCATEGORIES = ["Direction", "Stars", "Duration"];
+const SUBSUBCATEGORIES1 = ["Italy", "Spain", "Greece", "Turkey"];
+const SUBSUBCATEGORIES2 = [3, 4, 5];
+const SUBSUBCATEGORIES3 = ["5d/4n", "8d/7n", "12d/11n"];
 
 const NewUser = () => {
-  return (
-    <div>NewUser</div>
-  )
-}
+  // const [valuesList, setValuesList] = useState([]);
 
-export default NewUser
+  const [error, setError] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    passw: "",
+    age: "",
+    gender: "",
+    category: "",
+    subCategory: "",
+    subSubCategory: "",
+  });
+
+  const set = (name) => {
+    return ({ target: { value } }) => {
+      setValues((oldValues) => ({ ...oldValues, [name]: value }));
+      // console.log("values from set", values);
+    };
+  };
+
+  let arr = [];
+  const onSubmit = (event, data) => {
+    event.preventDefault();
+    setError(validation(values));
+    setIsSubmit(true);
+
+    arr = JSON.parse(localStorage.getItem("data", values)) || [];
+    arr.push(values);
+    // arr.push(values);
+    localStorage.setItem("data", JSON.stringify(arr));
+    console.log(arr);
+  };
+
+  useEffect(() => {
+    if (isSubmit) {
+      console.log(values);
+    }
+  }, [isSubmit]);
+
+  const [usdata, setDataLS] = useState("");
+
+  const getData = () => {
+    let data = JSON.parse(localStorage.getItem("data"));
+    console.log(data);
+    setDataLS(JSON.stringify(data, null, 4));
+    // return JSON.stringify(localStorage.getItem('data'));
+    // setDataLS(JSON.stringify(data));
+  };
+
+  //////////////////////
+
+  const [insert, setInsert] = useState(0);
+
+  const insertCreate = (donIsert) => {
+    setInsert(donIsert);
+    getData();
+  };
+  /////////////////////////
+
+  // const saveFormData = async () => {
+  //   const response = await fetch("http://localhost:5500/registration", {
+  //     method: "POST",
+  //     body: JSON.stringify(values),
+  //   });
+  //   console.log("response", response)
+  //   if (response.status !== 200) {
+  //     throw new Error(`Request failed: ${response.status}`);
+  //   }
+  // };
+
+  // const onSubmit = async (event) => {
+  //   event.preventDefault();
+  //   try {
+  //     await saveFormData();
+  //     alert("Your registration was successfully submitted!");
+  //     setValues({
+  //       firstName: "",
+  //       lastName: "",
+  //       email: "",
+  //       passw: "",
+  //       age: "",
+  //       gender: "",
+  //       category: "",
+  //     });
+  //   } catch (e) {
+  //     alert(`Registration failed! ${e.message}`);
+  //   }
+  // };
+
+  const validation = (val) => {
+    const errors = {};
+    if (!val.password) {
+      errors.password = "Please enter password!";
+    } else if (val.password.length < 7) {
+      errors.password = "Password must contain at least 7 characters!";
+    } else if (val.password.length > 25) {
+      errors.password = "Seems that password is too long";
+    }
+  };
+  // const getValuesOnSubmit = () => {};
+  ////////////////////////////////////////////////
+  // const [valuesList, setValuesList] = useState([]);
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [age, setAge] = useState("");
+  // const [gender, setGender] = useState("");
+  // const [category, setCategory] = useState("");
+  // const [error, setError] = useState({});
+
+  // const newName = (event) => {
+  //   console.log(event.target.value);
+  //   setFirstName(event.target.value);
+  // };
+  // const newLastName = (event) => {
+  //   console.log(event.target.value);
+  //   setLastName(event.target.value);
+  // };
+  // const newMail = (event) => {
+  //   console.log(event.target.value);
+  //   setEmail(event.target.value);
+  // };
+  // const newPassw = (event) => {
+  //   console.log(event.target.value);
+  //   setPassword(event.target.value);
+  // };
+  // const newAge = (event) => {
+  //   console.log(event.target.value);
+  //   setAge(event.target.value);
+  // };
+  // const newGender = (event) => {
+  //   console.log(event.target.value);
+  //   setGender(event.target.value);
+  // };
+  // const newCategory = (event) => {
+  //   console.log(event.target.value);
+  //   setCategory(event.target.value);
+  // };
+
+  // const addValues = (event) => {
+  //   event.preventDefault();
+  //   console.log(valuesList);
+  //   setValuesList((val) => [
+  //     ...val,
+  //     {
+  //       firstName: firstName,
+  //       lastName: lastName,
+  //       email: email,
+  //       passw: password,
+  //       age: age,
+  //       gender: gender,
+  //       category: category,
+  //     },
+  //   ]);
+  //   console.log(valuesList);
+  //   setFirstName("");
+  //   setLastName("");
+  //   setEmail("");
+  //   setPassword("");
+  //   setAge("");
+  //   setGender("");
+  //   setCategory("");
+  // };
+
+  // const valuesValidation = (values) => {
+  //   const errors = {};
+  //   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+  //   // firstname
+  //   if (!values.firstName) {
+  //     errors.firstName = "* Username is required";
+  //   }
+  // };
+
+  ///////////////////////////////////////////////////
+  return (
+    <>
+      <div>
+        {insert ? (
+          <Modal
+            usdata={usdata}
+            insert={insert}
+            setInsert={setInsert}
+          ></Modal>
+        ) : null}
+      </div>
+      <div>
+        <form onSubmit={onSubmit}>
+          <div>
+            <label>First name</label>
+            <input
+              type="text"
+              name="firstName"
+              value={values.firstName}
+              onChange={set("firstName")}
+              required
+            ></input>
+            {/* <p className="error-message">{error.firstName}</p> */}
+          </div>
+          <div>
+            <label>Last name</label>
+            <input
+              type="text"
+              name="lastName"
+              value={values.lastName}
+              onChange={set("lastName")}
+              required
+            ></input>
+            {/* <p className="error-message">{error.firstName}</p> */}
+          </div>
+          <div>
+            <label>Your e-mail</label>
+            <input
+              type="text"
+              name="email"
+              value={values.email}
+              onChange={set("email")}
+              required
+            ></input>
+            {/* <p className="error-message">{error.firstName}</p> */}
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              name="passw"
+              value={values.passw}
+              onChange={set("passw")}
+              required
+            ></input>
+            {/* <p className="error-message">{error.firstName}</p> */}
+          </div>
+          <div>
+            <label>Age</label>
+            <input
+              type="number"
+              name="age"
+              value={values.age}
+              onChange={set("age")}
+              required
+              min="0"
+            ></input>
+            {/* <p className="error-message">{error.firstName}</p> */}
+          </div>
+          <div>
+            <label>Gender</label>
+            <select value={values.gender} onChange={set("gender")} required>
+              <option value="">Choose...</option>
+              {GENDER.map((c) => (
+                <option key={c}>{c}</option>
+              ))}
+            </select>
+            {/* <select value={gender} onChange={newGender}>
+              <option value="choose">Choose...</option>
+              <option value="female">Female</option>
+              <option value="male">Male</option>
+              <option value="other">Other</option>
+            </select> */}
+            {/* <p className="error-message">{error.firstName}</p> */}
+          </div>
+          <div>
+            <label>Category</label>
+            <select value={values.category} onChange={set("category")} required>
+              <option value="">Choose...</option>
+              {CATEGORIES.map((c) => (
+                <option key={c}>{c}</option>
+              ))}
+            </select>
+
+            {/* <select value={category} onChange={newCategory}>
+              <option value="category">Choose...</option>
+              <option value="plain">By plain</option>
+              <option value="bus">By bus</option>
+              <option value="ship">By ship</option>
+            </select> */}
+            {/* <p className="error-message">{error.firstName}</p> */}
+          </div>
+
+          {/* <button type="submit" onClick={getValuesOnSubmit}> */}
+          <button type="submit">Prideti reiksme</button>
+        </form>
+
+        {/* <Link to="/data" onClick={getData} usdata={usdata}>
+            <p>getData{usdata}</p>
+          </Link> */}
+        {/* <pre onClick={getData}>
+          DATA
+          {usdata}
+        </pre> */}
+        <button
+          onClick={insertCreate}
+          type="button"
+          className="button header-button"
+        >
+          Get data
+        </button>
+
+        {/* <a href="_blank" onClick={getData}>getData</a> */}
+
+        {/* <Data data={data} /> */}
+
+        {/* <div>
+          {valuesList.map((el, i) => {
+            return <div key={i}>{el.firstName}{el.lastName}</div>;
+          })}
+        </div> */}
+      </div>
+    </>
+  );
+};
+
+export default NewUser;
