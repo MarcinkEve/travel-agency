8936883a11a3e9a29506c5bf6c639faa98324a08
MarcinkEve <marcinke@gmail.com>
2022-06-19 20:22:36 +0300

    Closing the dropdown menu when users click outside it

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,13 +1,30 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Dropdown from "../Dropdown/Dropdown";
 
 const Categories = ({ menu }) => {
   const [dropdown, setDropdown] = useState(false);
+  let ref = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+     if (dropdown && ref.current && !ref.current.contains(event.target)) {
+      setDropdown(false);
+     }
+    };
+    document.addEventListener("mousedown", handler);
+    document.addEventListener("touchstart", handler);
+    return () => {
+     // Cleanup the event listener
+     document.removeEventListener("mousedown", handler);
+     document.removeEventListener("touchstart", handler);
+    };
+   }, [dropdown]);
+
 
   return (
-    <div className="menu-items">
+    <div className="menu-items"  ref={ref}>
       {menu.submenu ? (
         <>
           <button
