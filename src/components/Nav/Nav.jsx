d1f8303abbe6483d7805c97e9c161c84cb1c96b5
MarcinkEve <marcinke@gmail.com>
2022-06-19 20:28:11 +0300

    Closing the dropdown menu when users click outside it- in main categories menu

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import "./nav.css";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { categItems } from "../categItems.js";
 import Categories from "../Categories/Categories";
 
 const Nav = () => {
   const [drop, setDrop] = useState(false);
+
+  let ref = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (drop && ref.current && !ref.current.contains(event.target)) {
+        setDrop(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+    document.addEventListener("touchstart", handler);
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", handler);
+      document.removeEventListener("touchstart", handler);
+    };
+  }, [drop]);
+
   return (
     <div>
       <ul>
@@ -25,7 +43,9 @@ const Nav = () => {
             <p>New Category</p>
           </Link>
         </li>
-        <li onClick={() => setDrop((prev) => !prev)} >Categories</li>
+        <li onClick={() => setDrop((prev) => !prev)} ref={ref}>
+          Categories
+        </li>
         <li className={`dropdown ${drop ? "show" : ""}`}>
           {categItems.map((menu, i) => {
             return (
