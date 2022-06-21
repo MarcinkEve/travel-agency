b3109c88f09ffa3ee2411653f60c313a74c86a96
MarcinkEve <marcinke@gmail.com>
2022-06-22 00:45:29 +0300

    'Dropdown' component gets className="dropdow__container"

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,20 +1,23 @@
 import React from "react";
+import "./dropdown.scss";
 
 const Dropdown = ({ submenus, dropdown, isNavExpanded, setIsNavExpanded }) => {
   return (
-    <ul className={`dropdown ${dropdown ? "show" : ""}`}>
-      {submenus.map((submenu, index) => (
-        <li
-          onClick={() => {
-            setIsNavExpanded(!isNavExpanded);
-          }}
-          key={index}
-          className="menu-items"
-        >
-          <a href="/#">{submenu.title}</a>
-        </li>
-      ))}
-    </ul>
+    <div className="dropdow__container">
+      <ul className={`dropdown ${dropdown ? "show" : ""}`}>
+        {submenus.map((submenu, index) => (
+          <li
+            onClick={() => {
+              setIsNavExpanded(!isNavExpanded);
+            }}
+            key={index}
+            className="menu-items"
+          >
+            <a href="/#">{submenu.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
     //     <ul className={`dropdown ${dropdownClass} ${dropdown ? "show" : ""}`}>
     //     {submenus.map((submenu, index) => (
     //      <Categories depthLevel={depthLevel} items={submenu} key={index} />
