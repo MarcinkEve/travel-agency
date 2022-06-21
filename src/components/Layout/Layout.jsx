46a1c185648bc9ebcb29ce324a6a94da7702aad8
MarcinkEve <marcinke@gmail.com>
2022-06-21 23:14:26 +0300

    "Burger" navigation menu symbol for mobile responsive design added

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import Nav from "../Nav/Nav";
 import "./layout.scss";
 
-const Layout = () => (
-  <div>
-    <Header />
-    <div className="content">
-      <Nav />
-      <Outlet/>
+const Layout = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+  return (
+    <div>
+      <Header
+        isNavExpanded={isNavExpanded}
+        setIsNavExpanded={setIsNavExpanded}
+      />
+      <div className="content">
+        <Nav
+          isNavExpanded={isNavExpanded}
+          setIsNavExpanded={setIsNavExpanded}
+        />
+        <Outlet />
+      </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
-);
+  );
+};
 
 export default Layout;
