4a9f3f5f21062eaab8d88f4a96f47917d417bb3a
MarcinkEve <marcinke@gmail.com>
2022-06-23 22:59:24 +0300

    'NewUser' component classNames and style added

--- src/components/NewUser/NewUser.jsx
+++ src/components/NewUser/NewUser.jsx
@@ -6,7 +6,7 @@ import { getFromLocal, setFromLocal } from "../../utils/utils";
 
 const GENDER = ["Female", "Male", "Other"];
 const DIRECTION = ["Italy", "Spain", "Greece", "Turkey"];
-const TRANSPORT = ["By plain", "By bus"];
+const TRANSPORT = ["Plain", "Bus", "Ship"];
 const DURATION = ["5d/4n", "8d/7n", "12d/11n"];
 // const TRANSPORT = ["Direction", "Stars", "Duration"];
 // const DURATION2 = [3, 4, 5];
@@ -106,165 +106,184 @@ const NewUser = () => {
   return (
     <>
       <div
-        style={{
-          backgroundImage:
-            "url(https://part.lt/img/c48728a54a9bd63bd7936d3653c14a30779.jpg)",
-        }}
+      // style={{
+      //   backgroundImage:
+      //     "url(https://part.lt/img/c48728a54a9bd63bd7936d3653c14a30779.jpg)",
+      // }}
       >
         {insert ? (
           <Modal usdata={usdata} insert={insert} setInsert={setInsert}></Modal>
         ) : null}
       </div>
-      <div>
-        <form onSubmit={onSubmit}>
-          <div>
-            <label>First name</label>
-            <input
-              type="text"
-              name="firstName"
-              value={values.firstName}
-              onChange={set("firstName")}
-              required
-            ></input>
-            {/* <p className="error-message">{error.firstName}</p> */}
+      <div className="user-container">
+        <div className="user-form__container">
+          <div className="user-form__container__header">
+            <h2 className="user-form__container__header--title">
+              Fill to create new user
+            </h2>
+            <div className="user-form__container__header--buttons">
+              <button
+                onClick={insertCreate}
+                type="button"
+                className="user-form__container__header--buttons-button"
+              >
+                Show users list
+              </button>
+
+              <button
+                onClick={downloadData}
+                type="button"
+                className="user-form__container__header--buttons-button"
+              >
+                Download list
+              </button>
+            </div>
           </div>
-          <div>
-            <label>Last name</label>
-            <input
-              type="text"
-              name="lastName"
-              value={values.lastName}
-              onChange={set("lastName")}
-              required
-            ></input>
-            {/* <p className="error-message">{error.firstName}</p> */}
-          </div>
-          <div>
-            <label>Your e-mail</label>
-            <input
-              type="text"
-              name="email"
-              value={values.email}
-              onChange={set("email")}
-              required
-            ></input>
-            {/* <p className="error-message">{error.firstName}</p> */}
-          </div>
-          <div>
-            <label>Password</label>
-            <input
-              type="password"
-              name="passw"
-              value={values.passw}
-              onChange={set("passw")}
-              required
-            ></input>
-            {/* <p className="error-message">{error.passw}</p> */}
-          </div>
-          <div>
-            <label>Age</label>
-            <input
-              type="number"
-              name="age"
-              value={values.age}
-              onChange={set("age")}
-              required
-              min="0"
-            ></input>
-            {/* <p className="error-message">{error.firstName}</p> */}
-          </div>
-          <div>
-            <label>Gender</label>
-            <select value={values.gender} onChange={set("gender")} required>
-              <option value="">Choose...</option>
-              {GENDER.map((c) => (
-                <option key={c}>{c}</option>
-              ))}
-            </select>
-            {/* <select value={gender} onChange={newGender}>
+          <form onSubmit={onSubmit} className="user-form">
+            <div className="user-form__side">
+              <div className="user-form__input">
+                <label className="user-form__input--title">First name</label>
+                <input
+                  type="text"
+                  name="firstName"
+                  value={values.firstName}
+                  onChange={set("firstName")}
+                  required
+                  className="user-form__input--field"
+                ></input>
+                {/* <p className="error-message">{error.firstName}</p> */}
+              </div>
+              <div className="user-form__input">
+                <label className="user-form__input--title">Last name</label>
+                <input
+                  type="text"
+                  name="lastName"
+                  value={values.lastName}
+                  onChange={set("lastName")}
+                  required
+                  className="user-form__input--field"
+                ></input>
+                {/* <p className="error-message">{error.firstName}</p> */}
+              </div>
+              <div className="user-form__input">
+                <label className="user-form__input--title">Your e-mail</label>
+                <input
+                  type="text"
+                  name="email"
+                  value={values.email}
+                  onChange={set("email")}
+                  required
+                  className="user-form__input--field"
+                ></input>
+                {/* <p className="error-message">{error.firstName}</p> */}
+              </div>
+              <div className="user-form__input">
+                <label className="user-form__input--title">Password</label>
+                <input
+                  type="password"
+                  name="passw"
+                  value={values.passw}
+                  onChange={set("passw")}
+                  required
+                  className="user-form__input--field"
+                ></input>
+                {/* <p className="error-message">{error.passw}</p> */}
+              </div>
+              <div className="user-form__input">
+                <label className="user-form__input--title">Age</label>
+                <input
+                  type="number"
+                  name="age"
+                  value={values.age}
+                  onChange={set("age")}
+                  required
+                  min="0"
+                  className="user-form__input--field"
+                ></input>
+                {/* <p className="error-message">{error.firstName}</p> */}
+              </div>
+            </div>
+            <div className="user-form__side">
+              <div className="user-form__input">
+                <label className="user-form__input--title">Gender</label>
+                <select
+                  value={values.gender}
+                  onChange={set("gender")}
+                  required
+                  className="user-form__input--select"
+                >
+                  <option value="">Choose...</option>
+                  {GENDER.map((c) => (
+                    <option key={c}>{c}</option>
+                  ))}
+                </select>
+                {/* <select value={gender} onChange={newGender}>
               <option value="choose">Choose...</option>
               <option value="female">Female</option>
               <option value="male">Male</option>
               <option value="other">Other</option>
             </select> */}
-            {/* <p className="error-message">{error.firstName}</p> */}
-          </div>
-          <div>
-            <label>Direction</label>
-            <select
-              value={values.direction}
-              onChange={set("direction")}
-              required
-            >
-              <option value="">Choose...</option>
-              {DIRECTION.map((c) => (
-                <option key={c}>{c}</option>
-              ))}
-            </select>
+                {/* <p className="error-message">{error.firstName}</p> */}
+              </div>
+              <div className="user-form__input">
+                <label className="user-form__input--title">Direction</label>
+                <select
+                  value={values.direction}
+                  onChange={set("direction")}
+                  required
+                  className="user-form__input--select"
+                >
+                  <option value="">Choose...</option>
+                  {DIRECTION.map((c) => (
+                    <option key={c}>{c}</option>
+                  ))}
+                </select>
 
-            {/* <select value={direction} onChange={newdirection}>
+                {/* <select value={direction} onChange={newdirection}>
               <option value="direction">Choose...</option>
               <option value="plain">By plain</option>
               <option value="bus">By bus</option>
               <option value="ship">By ship</option>
             </select> */}
-            {/* <p className="error-message">{error.firstName}</p> */}
-          </div>
+                {/* <p className="error-message">{error.firstName}</p> */}
+              </div>
 
-          <div>
-            <label>Transport</label>
-            <select
-              value={values.transport}
-              onChange={set("transport")}
-              required
-            >
-              <option value="">Choose...</option>
-              {TRANSPORT.map((c) => (
-                <option key={c}>{c}</option>
-              ))}
-            </select>
-            {/* <p className="error-message">{error.firstName}</p> */}
-          </div>
+              <div className="user-form__input">
+                <label className="user-form__input--title">Transport</label>
+                <select
+                  value={values.transport}
+                  onChange={set("transport")}
+                  required
+                  className="user-form__input--select"
+                >
+                  <option value="">Choose...</option>
+                  {TRANSPORT.map((c) => (
+                    <option key={c}>{c}</option>
+                  ))}
+                </select>
+                {/* <p className="error-message">{error.firstName}</p> */}
+              </div>
 
-          <div>
-            <label>Duration</label>
-            <select value={values.duration} onChange={set("duration")} required>
-              <option value="">Choose...</option>
-              {DURATION.map((c) => (
-                <option key={c}>{c}</option>
-              ))}
-            </select>
-            {/* <p className="error-message">{error.firstName}</p> */}
-          </div>
+              <div className="user-form__input">
+                <label className="user-form__input--title">Duration</label>
+                <select
+                  value={values.duration}
+                  onChange={set("duration")}
+                  required
+                  className="user-form__input--select"
+                >
+                  <option value="">Choose...</option>
+                  {DURATION.map((c) => (
+                    <option key={c}>{c}</option>
+                  ))}
+                </select>
+                {/* <p className="error-message">{error.firstName}</p> */}
+              </div>
 
-          <button type="submit">Add user</button>
-        </form>
-
-        <button
-          onClick={insertCreate}
-          type="button"
-          className="button header-button"
-        >
-          Show users list
-        </button>
-
-        <button
-          onClick={downloadData}
-          type="button"
-          className="button header-button"
-        >
-          Download list
-        </button>
-
-        {/* <a href="_blank" onClick={getData}>getData</a> */}
-
-        {/* <Data data={data} /> */}
-
-        {/* <div>
-          {valuesList.map((el, i) => {
-            return <div key={i}>{el.firstName}{el.lastName}</div>;
-          })}
-        </div> */}
+              <button type="submit" className="user-form__submitButton">Add user</button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
