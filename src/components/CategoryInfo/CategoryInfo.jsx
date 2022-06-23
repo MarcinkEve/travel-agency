e856d8ecc6631ab5aff201311e9cef3e08f34571
MarcinkEve <marcinke@gmail.com>
2022-06-24 01:01:26 +0300

    'Categoryinfo' component classNames  and style added

--- src/components/CategoryInfo/CategoryInfo.jsx
+++ src/components/CategoryInfo/CategoryInfo.jsx
@@ -1,14 +1,26 @@
 import React, { useMemo } from "react";
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { getFromLocal } from "../../utils/utils"
+import { getFromLocal } from "../../utils/utils";
+import "./categoryInfo.scss";
+
+const TABLEHEADING = [
+  "No",
+  "First Name",
+  "Last name",
+  "Age",
+  "E-mail",
+  "Gender",
+  "Direction",
+  "Transport",
+  "Duration",
+];
 
 const CategoryInfo = ({ category = "direction", value = "Spain" }) => {
   const [item, setItem] = useState([]);
 
   const { search } = useLocation();
 
-  // skirta keisti query url
   const query = useMemo(
     () => Object.fromEntries(new URLSearchParams(search)),
     [search]
@@ -17,31 +29,100 @@ const CategoryInfo = ({ category = "direction", value = "Spain" }) => {
   useEffect(() => {
     const items = getFromLocal("data");
     if (items) {
-      if(Object.keys(query).length){
+      if (Object.keys(query).length) {
         const filtering = items.filter((el, i) => {
           const key = Object.keys(query)[0];
           const prop = query[key];
-          console.log(value[prop]);
-          console.log(el[key?.toLowerCase()])
+          // console.log("val", value[prop]);
+          
           return el[key?.toLowerCase()] === prop;
         });
-        console.log(filtering.length);
+        console.log("filtering", filtering);
         setItem(filtering);
       }
     }
   }, [JSON.stringify(query)]);
 
+  const handleSaveToPC = (jsonData) => {
+    //////////
+    const items = getFromLocal("data");
+    if (items) {
+      if (Object.keys(query).length) {
+        const filtering = items.filter((el, i) => {
+          const key = Object.keys(query)[0];
+          const prop = query[key];
+          // console.log("val", value[prop]);
+          return el[key?.toLowerCase()] === prop;
+        });
+        
+        //////////////
+        
+        const fileData = JSON.stringify(filtering);
+        const blob = new Blob([fileData], { type: "text/plain" });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.download = "filename.txt";
+        link.href = url;
+        link.click();
+        console.log("jsonData", jsonData);
+        console.log("fileData", fileData);
+      }
+    }
+  };
+
+  const downloadData = () => {
+    handleSaveToPC(JSON.parse(localStorage.getItem("data"), null, 4));
+  };
+
   return (
     <>
-      <div style={{ background: "red" }}>CategoryInfo</div>
-      <button>getgetgetget</button>
-      <div>
-        {item.map((el, i) => (
-          <div key={i}>
-            <strong>{el.firstName}</strong>
-            <small>{el.lastName}</small>
-          </div>
-        ))}
+      <div className="categoryInfo-wrapper">
+        <h2 className="categoryInfo--title">
+          User list according selected subcategory
+        </h2>
+        <button
+          onClick={downloadData}
+          type="button"
+          className="user-form__container__header--buttons-button"
+        >
+          Download list
+        </button>
+        <table className="categoryInfo__table">
+          <thead>
+            <tr className="categoryInfo__table__body">
+              {TABLEHEADING.map((c) => (
+                <th key={c} className="categoryInfo__table__body__heading">
+                  {c}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {item.map((el, i) => (
+              <tr key={i} className="categoryInfo__table__body">
+                <td className="categoryInfo__table__body__data">{++i}</td>
+                <td className="categoryInfo__table__body__data">
+                  {el.firstName}
+                </td>
+                <td className="categoryInfo__table__body__data">
+                  {el.lastName}
+                </td>
+                <td className="categoryInfo__table__body__data">{el.age}</td>
+                <td className="categoryInfo__table__body__data">{el.email}</td>
+                <td className="categoryInfo__table__body__data">{el.gender}</td>
+                <td className="categoryInfo__table__body__data">
+                  {el.direction}
+                </td>
+                <td className="categoryInfo__table__body__data">
+                  {el.transport}
+                </td>
+                <td className="categoryInfo__table__body__data">
+                  {el.duration}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
