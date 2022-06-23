430b89e0a1f7b11daa67f7fa67f5a66c5c62f77c
MarcinkEve <marcinke@gmail.com>
2022-06-24 01:03:56 +0300

    'Categoryinfo' component last changes deleted

--- src/components/CategoryInfo/CategoryInfo.jsx
+++ src/components/CategoryInfo/CategoryInfo.jsx
@@ -34,45 +34,14 @@ const CategoryInfo = ({ category = "direction", value = "Spain" }) => {
           const key = Object.keys(query)[0];
           const prop = query[key];
           // console.log("val", value[prop]);
-          
+          // console.log("el", el[key?.toLowerCase()]);
           return el[key?.toLowerCase()] === prop;
         });
-        console.log("filtering", filtering);
+        // console.log(filtering.length);
         setItem(filtering);
       }
     }
   }, [JSON.stringify(query)]);
-
-  const handleSaveToPC = (jsonData) => {
-    //////////
-    const items = getFromLocal("data");
-    if (items) {
-      if (Object.keys(query).length) {
-        const filtering = items.filter((el, i) => {
-          const key = Object.keys(query)[0];
-          const prop = query[key];
-          // console.log("val", value[prop]);
-          return el[key?.toLowerCase()] === prop;
-        });
-        
-        //////////////
-        
-        const fileData = JSON.stringify(filtering);
-        const blob = new Blob([fileData], { type: "text/plain" });
-        const url = URL.createObjectURL(blob);
-        const link = document.createElement("a");
-        link.download = "filename.txt";
-        link.href = url;
-        link.click();
-        console.log("jsonData", jsonData);
-        console.log("fileData", fileData);
-      }
-    }
-  };
-
-  const downloadData = () => {
-    handleSaveToPC(JSON.parse(localStorage.getItem("data"), null, 4));
-  };
 
   return (
     <>
@@ -80,13 +49,6 @@ const CategoryInfo = ({ category = "direction", value = "Spain" }) => {
         <h2 className="categoryInfo--title">
           User list according selected subcategory
         </h2>
-        <button
-          onClick={downloadData}
-          type="button"
-          className="user-form__container__header--buttons-button"
-        >
-          Download list
-        </button>
         <table className="categoryInfo__table">
           <thead>
             <tr className="categoryInfo__table__body">
