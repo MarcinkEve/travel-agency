ec3c2b362cbebb379a37c5becdaa12a4795e5140
MarcinkEve <marcinke@gmail.com>
2022-06-24 13:58:20 +0300

    Component 'Dropdown': removed commented lines

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,24 +1,16 @@
 import React from "react";
 import "./dropdown.scss";
-import { Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Dropdown = ({ submenus, dropdown, isNavExpanded, setIsNavExpanded, category }) => {
+const Dropdown = ({ submenus, dropdown, category }) => {
   return (
     <div className="dropdown__container">
       <ul className={`dropdown ${dropdown ? "show" : ""}`}>
         {submenus.map((submenu, index) => (
-          <li
-            // onClick={() => {
-            //   setIsNavExpanded(!isNavExpanded);
-            //   console.log("submenus", submenu);
-            //   // console.log("dropdown", dropdown);
-            //   // console.log("category", category);
-            //   window.location.replace(`categoryInfo?${category}=${submenu.title}`)
-            // }}
-            key={index}
-            className="menu-items"
-          >
-            <Link to={`categoryInfo?${category}=${submenu.title}`}>{submenu.title}</Link>
+          <li key={index} className="menu-items">
+            <Link to={`categoryInfo?${category}=${submenu.title}`}>
+              {submenu.title}
+            </Link>
           </li>
         ))}
       </ul>
