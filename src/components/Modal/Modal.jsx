f712103d58ee57d721be1b8d123f7421531f2678
MarcinkEve <marcinke@gmail.com>
2022-06-24 14:02:48 +0300

    'Modal' component: removed commented lines

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -7,9 +7,14 @@ const Modal = ({ usdata, setInsert }) => {
     <>
       <div className="modal">
         <div className="modal__close">
-          {/* this is "Close" button */}
-          <button onClick={() => setInsert(0)} type="button" className="modal__close--button">
-            <span aria-hidden="true" className="modal__close--button-X">&times;</span>
+          <button
+            onClick={() => setInsert(0)}
+            type="button"
+            className="modal__close--button"
+          >
+            <span aria-hidden="true" className="modal__close--button-X">
+              &times;
+            </span>
           </button>
         </div>
         <div className="modal__data">
